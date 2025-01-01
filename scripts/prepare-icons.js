c180bcdd91f509bf735e705983c468b580692c36
EbadiDev <kingali0098@gmail.com>
2025-01-02 00:39:28 +0330

    Fix Windows icon conversion using png-to-ico

--- scripts/prepare-icons.js
+++ scripts/prepare-icons.js
@@ -1,10 +1,11 @@
 const sharp = require('sharp');
 const path = require('path');
 const fs = require('fs');
+const pngToIco = require('png-to-ico');
 
 async function convertIcons() {
     const sourceImage = path.join(__dirname, '../resources/logo/logo.jpg');
-    const sizes = [16, 32, 48, 64, 128, 256, 512, 1024];
+    const sizes = [16, 32, 48, 64, 128, 256];
     
     // Create build/icons directory if it doesn't exist
     const iconDir = path.join(__dirname, '../build/icons');
@@ -12,29 +13,34 @@ async function convertIcons() {
         fs.mkdirSync(iconDir, { recursive: true });
     }
     
-    // Create PNG icons for Linux
+    // Create PNG icons for Linux and Windows
     await sharp(sourceImage)
         .resize(512, 512)
         .png()
         .toFile(path.join(__dirname, '../build/icons/512x512.png'));
 
-    // Create ICO file for Windows
-    const windows = [];
+    // Create smaller PNGs for Windows ICO
     for (const size of sizes) {
-        windows.push(
-            sharp(sourceImage)
-                .resize(size, size)
-                .toBuffer()
-        );
+        await sharp(sourceImage)
+            .resize(size, size)
+            .png()
+            .toFile(path.join(__dirname, `../build/icons/${size}x${size}.png`));
     }
-    
-    const windowsBuffers = await Promise.all(windows);
-    await sharp(windowsBuffers[7])  // Use the 1024x1024 version
-        .toFile(path.join(__dirname, '../build/icons/icon.ico'));
+
+    // Create ICO file for Windows using the generated PNGs
+    const pngFiles = sizes.map(size => path.join(__dirname, `../build/icons/${size}x${size}.png`));
+    const icoBuffer = await pngToIco(pngFiles);
+    fs.writeFileSync(path.join(__dirname, '../build/icons/icon.ico'), icoBuffer);
+
+    // Clean up temporary PNG files
+    for (const size of sizes) {
+        fs.unlinkSync(path.join(__dirname, `../build/icons/${size}x${size}.png`));
+    }
 
     // Create ICNS for macOS
     await sharp(sourceImage)
         .resize(1024, 1024)
+        .png()
         .toFile(path.join(__dirname, '../build/icons/icon.icns'));
 
     console.log('Icons generated successfully');
